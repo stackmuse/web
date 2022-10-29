7cd1b7a2d10462d21bf5fc0a90d3b4c6eb1eed4b
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-10-29 12:39:35 -0500

    Create a user object

---
+++ src/lib/User.js
@@ -0,0 +1,34 @@
+class User {
+  storage = localStorage;
+  key = 'user';
+  data = {};
+
+  constructor() {
+    this.#load();
+  }
+
+  #load() {
+    const data = JSON.parse(this.storage.getItem(this.key));
+    this.data = data == null ? {} : data;
+  }
+
+  #dump() {
+    this.storage.setItem(this.key, JSON.stringify(this.data));
+  }
+
+  isAuthenticated() {
+    return this.data.username != null;
+  }
+
+  login(username, password) {
+    this.data.username = username;
+    this.#dump();
+  }
+
+  logout() {
+    this.data = {};
+    this.#dump();
+  }
+}
+
+export default User;
